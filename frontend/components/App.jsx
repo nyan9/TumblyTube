b01e2aea4ef1a024cc0fda00a2ff0cfa7d3ef08a
nyan9 <rynnaing@gmail.com>
2021-03-16 17:56:48 -0400

    entry point and react router

---
+++ frontend/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>TumblyTube</h1>
+    </div>
+  );
+};
+
+export default App;
--- frontend/tumblytube.jsx
+++ frontend/tumblytube.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
   const store = configureStore();
-
+  // TESTING START
   window.store = store;
-  ReactDOM.render(<h1>Welcome to TumblyTube⏯</h1>, root);
+  // TESTING END
+  
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store} />, root);
 });
