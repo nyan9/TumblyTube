a2775a646996b4f27a462e21e9f9ba8a04b81bba
nyan9 <rynnaing@gmail.com>
2021-03-16 17:37:50 -0400

    add store & thunk

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  context: __dirname,
   entry: "./frontend/tumblytube.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
