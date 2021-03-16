3233f7dc61bd730c5db45ac05a0bf3c0eb3d5aa5
nyan9 <rynnaing@gmail.com>
2021-03-16 13:06:12 -0400

    add backend auth

---
+++ app/models/user.rb
@@ -0,0 +1,52 @@
+class User < ApplicationRecord
+  
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    if user && user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_obj = BCrypt::Password.new(self.password_digest)
+    password_obj.is_password?(password)
+  end
+
+  def reset_session_token!
+    generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  private
+
+  def new_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def generate_unique_session_token
+    self.session_token = new_session_token
+    while User.find_by_session_token(self.session_token)
+      self.session_token = new_session_token
+    end
+    self.session_token
+  end
+end
