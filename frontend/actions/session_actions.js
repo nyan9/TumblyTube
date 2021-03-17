a7359cccd33477a86fa539fa314415f1506fc65d
nyan9 <rynnaing@gmail.com>
2021-03-17 10:41:50 -0400

    fix error render

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -25,15 +25,15 @@ const receiveErrors = (errors) => {
 };
 
 export const signup = (user) => (dispatch) => {
-  return (
-    APIUtil.signup(user).then((user) => dispatch(receiveCurrentUser(user))),
+  return APIUtil.signup(user).then(
+    (user) => dispatch(receiveCurrentUser(user)),
     (err) => dispatch(receiveErrors(err.responseJSON))
   );
 };
 
 export const login = (user) => (dispatch) => {
-  return (
-    APIUtil.login(user).then((user) => dispatch(receiveCurrentUser(user))),
+  return APIUtil.login(user).then(
+    (user) => dispatch(receiveCurrentUser(user)),
     (err) => dispatch(receiveErrors(err.responseJSON))
   );
 };
