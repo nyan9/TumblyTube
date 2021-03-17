8bc9c9b3cf2cbe9cf76431fc53f3b12c3da31a88
nyan9 <rynnaing@gmail.com>
2021-03-16 23:51:50 -0400

    add sessionFormComponent and session routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
 
 const App = () => {
   return (
     <div>
       <header>
-        <h1>TumblyTube</h1>  
+        <h1>TumblyTube</h1>
         <GreetingContainer />
       </header>
+
+      <Route path="/login" component={LoginFormContainer} />
+      <Route path="/signup" component={SignupFormContainer} />
     </div>
   );
 };
---
+++ frontend/components/session_form/signup_form_container.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import SessionForm from "./session_form";
+import { signup } from "../../actions/session_actions";
+
+const mSTP = ({ errors }) => {
+  return {
+    errors: errors.session,
+    formType: "signup",
+    formLink: <Link to="/login">Sign in instead</Link>,
+  };
+};
+
+const mDTP = (dispatch) => {
+  return {
+    action: (user) => dispatch(signup(user)),
+  };
+};
+
+export default connect(mSTP, mDTP)(SessionForm);
