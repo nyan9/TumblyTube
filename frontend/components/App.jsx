f148ee6f12a8ece01715ab94f7430419a9cc62b3
nyan9 <rynnaing@gmail.com>
2021-03-17 13:27:48 -0400

    add protected session routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => {
   return (
@@ -12,8 +13,8 @@ const App = () => {
         <GreetingContainer />
       </header>
 
-      <Route path="/login" component={LoginFormContainer} />
-      <Route path="/signup" component={SignupFormContainer} />
+      <AuthRoute path="/login" component={LoginFormContainer} />
+      <AuthRoute path="/signup" component={SignupFormContainer} />
     </div>
   );
 };
---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from "react-redux";
+import { Redirect, withRouter, Route } from "react-router";
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={(props) => (
+      loggedIn ? <Redirect to="/" /> : <Component {...props} />
+    )}
+  />
+);
+
+const mSTP = (state) => {
+  return { loggedIn: Boolean(state.session.id) };
+};
+
+export const AuthRoute = withRouter(
+  connect(mSTP, null)(Auth)
+);
