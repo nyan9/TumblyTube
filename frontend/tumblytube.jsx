71963dbbf05011cf6e57cee830dee68dc80f278d
nyan9 <rynnaing@gmail.com>
2021-03-17 13:06:33 -0400

    bootstrap current_user to windown

--- frontend/tumblytube.jsx
+++ frontend/tumblytube.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from './components/root';
+// TESTS
 import { login, logout, signup } from './actions/session_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id },
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   // TESTING START
   window.getState = store.getState;
@@ -14,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.login = login;
   window.signup = signup;
   // TESTING END
-  
+
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
