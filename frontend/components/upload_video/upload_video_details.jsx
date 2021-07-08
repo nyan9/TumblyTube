f361eb807029bdf903bd3fef0b1a0df420fc724a
nyan9 <rynnaing@gmail.com>
2021-07-08 00:48:20 -0400

    started video upload details

--- frontend/components/upload_video/upload_video_details.jsx
+++ frontend/components/upload_video/upload_video_details.jsx
@@ -1,18 +1,18 @@
 import React from "react";
+import CloseIcon from "@material-ui/icons/Close";
 
 export default class UploadVideoDetails extends React.Component {
   render() {
     return (
-      <main id="upload-video-form-container">
-        <header id="upload-video-details">
-          <span>{this.state.title}</span>
-          <div>
-            <i className="fas fa-exclamation-triangle"></i>
-            <i onClick={this.props.closeModal} className="fas fa-times"></i>
+      <div className="vid-upload">
+        <header className="vid-upload__header">
+          <span className="vid-upload__header__title ">Details</span>
+          <div className="vid-upload__header__btns">
+            <CloseIcon id="upload-close-btn" onClick={this.props.closeModal} />
           </div>
         </header>
 
-        <div id="line-container">
+        {/* <div id="line-container">
           <div id="circle-details-container">
             <p className="strong-p">Details</p>
             <div id="circle-details"></div>
@@ -25,51 +25,46 @@ export default class UploadVideoDetails extends React.Component {
             <p className="strong-p">Visibility</p>
             <div id="circle-visibility"></div>
           </div>
-        </div>
+        </div> */}
 
-        <div id="line"></div>
-
-        <h1>Details</h1>
-
-        <div id="video-details-form-container">
+        <div>
           <form>
             <input
               type="text"
               id="video-upload-title-input"
-              onChange={this.handleChange("title")}
-              value={this.state.title}
+              onChange={this.props.handleChange("title")}
+              value={this.props.title}
               placeholder="Add a title that describes your video"
               required
             />
-            <label id="video-upload-title-label">Title (required)</label>
+            <label>Title (required)</label>
             <textarea
               cols="30"
               rows="10"
-              onChange={this.handleChange("description")}
-              value={this.state.description}
+              onChange={this.props.handleChange("description")}
+              value={this.props.description}
               placeholder="Tell viewers about your video"
             ></textarea>
-            <label id="video-upload-description-label">Description</label>
+            <label>Description</label>
           </form>
-          <div id="video-upload-preview-container">
+          <div >
             <video height="170" width="303" controls>
-              <source src={this.state.videoUrl} type="video/mp4" />
-              <source src={this.state.videoUrl} type="video/ogg" />
-              <source src={this.state.videoUrl} type="video/webm" />
+              <source src={this.props.videoUrl} type="video/mp4" />
+              <source src={this.props.videoUrl} type="video/ogg" />
+              <source src={this.props.videoUrl} type="video/webm" />
               There was a problem rendering the video
             </video>
-            <div id="video-upload-preview-details-container">
-              <div id="video-preview-link-container">
+            <div >
+              <div>
                 <div>
                   <label>Video Link</label>
                   <a>Availble after successful upload</a>
                 </div>
-                <i className="far fa-copy"></i>
               </div>
-              <div id="video-preview-filename-container">
+              <div>
                 <div>
                   <label>Filename</label>
-                  <p className="strong-p">{fileName}</p>
+                  <p>{this.props.fileName}</p>
                 </div>
               </div>
             </div>
@@ -78,14 +73,13 @@ export default class UploadVideoDetails extends React.Component {
 
         <footer>
           <div>
-            <i className="fab fa-js-square"></i>
-            <p className="weak-p">Finished processing</p>
+            <p>Finished processing</p>
           </div>
           <button id="upload-video-submit-btn" onClick={this.handleSubmit}>
             {this.props.formType}
           </button>
         </footer>
-      </main>
+      </div>
     );
   }
 }
