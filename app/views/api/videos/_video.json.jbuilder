8ddecfe003b7494c0ebb9c156bb18c28198604ec
nyan9 <rynnaing@gmail.com>
2021-07-31 19:53:43 -0400

    add side videos component

--- app/views/api/videos/_video.json.jbuilder
+++ app/views/api/videos/_video.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! video, :id, :creator_id, :title, :description
+json.extract! video, :id, :creator_id, :title, :description, :created_at
+json.extract! video.creator, :username
 json.videoUrl url_for(video.video_file)
--- frontend/components/show_video/side_vid_idx.jsx
+++ frontend/components/show_video/side_vid_idx.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function SideVideoIndex(props) {
+  function handleMouseEnter(e) {
+    e.target.play();
+  }
+
+  function handleMouseOut(e) {
+    e.target.currentTime = 0;
+    e.target.pause();
+  }
+
   return (
     <ul>
       <li>
         <Link to={`/videos/${props.video.id}`}>
-          <video height='170' width='303' src={props.video.videoUrl}></video>
+          <video
+            height='170'
+            width='303'
+            src={props.video.videoUrl}
+            onMouseEnter={handleMouseEnter}
+            onMouseOut={handleMouseOut}
+          ></video>
           {props.video.title}
+          {props.video.username}
+          {props.video.created_at}
         </Link>
       </li>
-      ))
     </ul>
   );
 }
