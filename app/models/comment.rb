92253fdc2c45ad671295d31c2696a7305ad9b1fd
nyan9 <rynnaing@gmail.com>
2021-08-02 16:23:43 -0400

    run annotate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id                :bigint           not null, primary key
+#  body              :text             not null
+#  video_id          :integer          not null
+#  commenter_id      :integer          not null
+#  parent_comment_id :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 class Comment < ApplicationRecord
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id                :bigint           not null, primary key
+#  body              :text             not null
+#  video_id          :integer          not null
+#  commenter_id      :integer          not null
+#  parent_comment_id :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
