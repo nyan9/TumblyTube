cbe5d6d97b467f61adfa51015283eaa3a773214d
nyan9 <rynnaing@gmail.com>
2021-08-04 23:47:43 -0400

    add comments controller methods

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,9 +1,26 @@
 class Api::CommentsController < ApplicationController
+  
+  def index 
+    @comments = Comment.all
+    render :index
+  end
+  
   def create
+    @comment = current_user.comments.new(comment_params)
 
+    if @comment.save!
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 401
+    end
   end
 
   def destroy
-    
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :video_id, :parent_comment_id)
   end
 end
